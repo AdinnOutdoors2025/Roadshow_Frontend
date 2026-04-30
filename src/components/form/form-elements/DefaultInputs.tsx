524391031849802e3e2777a8389fe734b310dbae
karthika <reactdeveloper@adinn.co.in>
2026-04-30 17:41:46 +0530

    Onboarding_Page_DeleteReadMe&Licence

--- src/components/form/form-elements/DefaultInputs.tsx
+++ src/components/form/form-elements/DefaultInputs.tsx
@@ -39,7 +39,7 @@ export default function DefaultInputs() {
           />
              <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
               <ChevronDownIcon/>
-            </span>
+            </span> 
           </div>
         </div>
         <div>
