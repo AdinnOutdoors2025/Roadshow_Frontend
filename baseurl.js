f748bcd2c3599910fa2695c9922d9f8786496f96
karthika <reactdeveloper@adinn.co.in>
2026-05-11 16:35:42 +0530

    .env file added, baseUrl Changed

--- baseurl.js
+++ baseurl.js
@@ -1,3 +1,5 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+// const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_LIVE;
+
 
 export default API_BASE;
