5144ccce438dfa136d8d82e84e6d5ce7bac4d85c
karthika <reactdeveloper@adinn.co.in>
2026-05-11 16:08:38 +0530

    "use client"; Removed

--- src/app/(admin)/admin/order-creation/page.tsx
+++ src/app/(admin)/admin/order-creation/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import AdminOrderForm from "./AdminOrderForm";
 import { HiOutlineShoppingBag, HiOutlineEye } from "react-icons/hi";
--- src/app/(admin)/admin/package-management/page.tsx
+++ src/app/(admin)/admin/package-management/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useEffect, useState } from "react";
 import PackageFormModal from "./PackageFormModal";
 import DeleteConfirmModal from "./DeleteModal";
